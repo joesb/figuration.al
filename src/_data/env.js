8c486cffa63c3c893487669674b3ab1c6ff9a6a8
Joe Baker <joesbaker@gmail.com>
2024-11-13 15:28:20 +0000

    Environment vars for GA

---
+++ src/_data/env.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+const environment = process.env.ELEVENTY_ENV;
+const PROD_ENV = 'production';
+const prodUrl = 'https://figuration.al';
+const devUrl = 'http://localhost:8080';
+const baseUrl = environment === PROD_ENV ? prodUrl : devUrl;
+const isProd = environment === PROD_ENV;
+
+export default {
+  environment,
+  isProd,
+  base: {
+      site: baseUrl,
+  },
+};
